789d6d76fa62215fab3410b4777964804295ac8f
Amir Hossein Goodarzi (Numb) <amir007ag@gmail.com>
2016-06-27 19:18:05 +0430

    rename the project

--- submee/manage.py
+++ submee/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tehlug_presenter.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "submee.settings")
 
     from django.core.management import execute_from_command_line
 
