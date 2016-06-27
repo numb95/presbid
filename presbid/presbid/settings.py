05004f33d4411c00a465f0143f103d5c303774b7
Amir Hossein Goodarzi (Numb) <amir007ag@gmail.com>
2016-06-28 01:15:21 +0430

    testing settings.py

--- presbid/presbid/settings.py
+++ presbid/presbid/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'presentation',
+    'presbid.presentation',
+    'presbid.accounts',
 ]
 
 MIDDLEWARE_CLASSES = [
--- presbid/presbid/urls.py
+++ presbid/presbid/urls.py
@@ -18,4 +18,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^register/$', 'accounts.views.register'),
+    url(r'^login/$', 'accounts.views.login'),
+    url(r'^logout/$', 'accounts.views.logout'),
+
 ]
