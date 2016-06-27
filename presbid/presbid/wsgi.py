6fb656a498284f51b6167eed3ee3af5515748e39
Amir Hossein Goodarzi (Numb) <amir007ag@gmail.com>
2016-06-27 19:33:22 +0430

    rename the project

--- presbid/presbid/wsgi.py
+++ presbid/presbid/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for submee project.
+WSGI config for presbid project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "submee.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "presbid.settings")
 
 application = get_wsgi_application()
